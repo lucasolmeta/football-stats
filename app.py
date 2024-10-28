93ade42470673311d4dca70badb428ed2bab0439
lucasolmeta <lolmetab@gmail.com>
2024-10-27 22:44:35 -0500

    big fixes

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 import pandas as pd
 import requests
 import os
+
 RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")
 
 app = Flask(__name__)
@@ -27,4 +28,4 @@ def get_data():
         return jsonify({"error": str(err)}), 500
     
 if __name__ == '__main__':
-    app.run(debug=True)
+        app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
