12663269ea7dde6a62a3df43bc28de7a30b1eeee
lucasolmeta <lolmetab@gmail.com>
2024-10-29 00:55:11 -0500

    front end cleanup

--- front-end/script.mjs
+++ front-end/script.mjs
@@ -3,6 +3,19 @@ window.addEventListener('resize', resizeScreen);
 resizeScreen();
 
 function resizeScreen (){
+    const linkedIn = document.getElementById('linkedInButton');
+    const linkedInHeight = window.innerHeight/27*2;
+
+    if(window.innerHeight<1050){
+        linkedIn.style.marginTop = linkedInHeight/6 + 'px';
+        linkedIn.style.marginRight = linkedInHeight/6 + 'px';
+        linkedIn.style.height = linkedInHeight + 'px';
+    } else {
+        linkedIn.style.marginTop = '13px';
+        linkedIn.style.marginRight = '13px';
+        linkedIn.style.height = '52px';
+    }
+
     const searchBar = document.getElementById('searchBar');
 
     const searchBarWidth = window.innerWidth/1.9;
