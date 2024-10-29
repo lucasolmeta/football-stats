b5f7c315f05d75d41e03c8bc2b146e359896474a
lucasolmeta <lolmetab@gmail.com>
2024-10-29 01:23:48 -0500

    search bar work

--- front-end/script.mjs
+++ front-end/script.mjs
@@ -1,8 +1,30 @@
+const searchBar = document.getElementById('searchBar');
+
+searchBar.addEventListener('keydown', submissionMade);
 window.addEventListener('resize', resizeScreen);
 
 resizeScreen();
 
+function submissionMade(e){
+    if(e.key=='Enter'){
+        let searchQuery = document.getElementById('searchBar').value;
+
+        const regex = /^[a-zA-Z\s\-]+$/;
+        
+        if(!regex.test(searchQuery)){
+            document.getElementById('errorField').innerHTML = "Only letters, spaces, and hyphens are allowed!";
+            return;
+        }
+        else{
+            document.getElementById('errorField').innerHTML = "Success!";
+        }
+    }
+}
+
 function resizeScreen (){
+
+    //-------------- SET LINKEDIN BUTTON PROPERTIES --------------//
+
     const linkedIn = document.getElementById('linkedInButton');
     const linkedInHeight = window.innerHeight/27*2;
 
@@ -16,19 +38,36 @@ function resizeScreen (){
         linkedIn.style.height = '52px';
     }
 
+    //-------------- SET SEARCH BAR PROPERTIES --------------//
+
     const searchBar = document.getElementById('searchBar');
 
     const searchBarWidth = window.innerWidth/1.9;
     const searchBarHeight = window.innerHeight/12;
-    const searchBarMarginTop = window.innerHeight/2 - searchBarHeight/2;
-    const searchBarMarginLeft = window.innerWidth/2 - searchBarWidth/2;
+    const searchBarTop = window.innerHeight/2 - searchBarHeight/2;
+    const searchBarLeft = window.innerWidth/2 - searchBarWidth/2;
 
     searchBar.style.width = searchBarWidth + 'px';
     searchBar.style.height = searchBarHeight + 'px';
+    searchBar.style.lineHeight = searchBarHeight + 'px';
     searchBar.style.borderRadius = searchBarHeight + 'px';
     searchBar.style.fontSize = searchBarHeight*0.45 + 'px';
     searchBar.style.paddingLeft = searchBarHeight/2 + 'px';
-    searchBar.style.marginTop = searchBarMarginTop + 'px';
-    searchBar.style.marginLeft = searchBarMarginLeft + 'px';
-    searchBar.style.marginRight = searchBarMarginLeft + 'px';
+    searchBar.style.top = searchBarTop + 'px';
+    searchBar.style.left = searchBarLeft + 'px';
+
+    //-------------- SET ERROR FIELD PROPERTIES --------------//
+
+    const errorField = document.getElementById('errorField');
+
+    const errorFieldTop = searchBarTop + searchBarHeight + 5;
+
+    errorField.style.width = searchBarWidth + 'px';
+    errorField.style.height = searchBarHeight/4 + 'px';
+    errorField.style.lineHeight = searchBarHeight/4 + 'px';
+    errorField.style.fontSize = searchBarHeight*0.25 + 'px';
+    errorField.style.paddingLeft = searchBarHeight/2 + 'px';
+    errorField.style.paddingTop = searchBarHeight/16 + 'px';
+    errorField.style.top = errorFieldTop + 'px';
+    errorField.style.left = searchBarLeft + 'px';
 }
