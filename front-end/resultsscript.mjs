f9cc90ca279f676201afea2ece774f73aed934dc
lucasolmeta <lolmetab@gmail.com>
2024-11-12 22:03:45 -0600

    added error functionality

--- front-end/resultsscript.mjs
+++ front-end/resultsscript.mjs
@@ -18,166 +18,170 @@ function buildDisplay(){
 
     if (!data || !data.player) {
         basicInfo.style.color = 'red';
-        basicInfo.innerHTML = "No Advanced Information Found";
-        return;
-    }
+        basicInfo.innerHTML = "No Basic Information Found";
+    } else {
 
-    basicInfo.innerHTML = "<span class='title'>BASIC INFO: </span><br><br>";
+        basicInfo.innerHTML = "<span class='title'>BASIC INFO: </span><br><br>";
 
-    if(data.player.name != undefined){
-        let name = "NAME: <span class='answer'>";
+        if(data.player.name != undefined){
+         let name = "NAME: <span class='answer'>";
 
-        name += data.player.name;
-        name += "</span><br>";
+            name += data.player.name;
+            name += "</span><br>";
 
-        basicInfo.innerHTML += name;
-    }
+            basicInfo.innerHTML += name;
+        }
 
-    if(data.statistics[0].team.name != undefined){
-        let club = "CLUB: <span class='answer'>";
+        if(data.statistics[0].team.name != undefined){
+            let club = "CLUB: <span class='answer'>";
 
-        club += data.statistics[0].team.name;
-        club += "</span><br>";
+            club += data.statistics[0].team.name;
+            club += "</span><br>";
 
-        basicInfo.innerHTML += club;
-    }
+            basicInfo.innerHTML += club;
+        }
 
-    if(data.player.nationality != undefined){
-        let country = "COUNTRY: <span class='answer'>";
+        if(data.player.nationality != undefined){
+            let country = "COUNTRY: <span class='answer'>";
 
-        country += data.player.nationality;
-        country += "</span><br>";
+            country += data.player.nationality;
+            country += "</span><br>";
 
-        basicInfo.innerHTML += country;
-    }
+            basicInfo.innerHTML += country;
+        }
 
-    if(data.statistics[0].games.position != undefined){
-        let position = "POSITION: <span class='answer'>";
+        if(data.statistics[0].games.position != undefined){
+            let position = "POSITION: <span class='answer'>";
 
-        position += data.statistics[0].games.position;
-        position += "</span><br>";
+            position += data.statistics[0].games.position;
+            position += "</span><br>";
 
-        basicInfo.innerHTML += position;
-    }
+            basicInfo.innerHTML += position;
+        }
 
-    if(data.player.age != undefined){
-        let age = "AGE: <span class='answer'>";
+        if(data.player.age != undefined){
+            let age = "AGE: <span class='answer'>";
 
-        age += data.player.age;
-        age += "</span><br>";
+            age += data.player.age;
+            age += "</span><br>";
 
-        basicInfo.innerHTML += age;
-    }
+            basicInfo.innerHTML += age;
+        }
 
-    if(data.player.height != undefined){
-        let height = "HEIGHT: <span class='answer'>";
+        if(data.player.height != undefined){
+            let height = "HEIGHT: <span class='answer'>";
 
-        height += data.player.height;
-        height += "</span><br>";
+            height += data.player.height;
+            height += "</span><br>";
 
-        basicInfo.innerHTML += height;
-    }
+            basicInfo.innerHTML += height;
+        }
 
-    if(data.player.weight != undefined){
-        let weight = "WEIGHT: <span class='answer'>";
+        if(data.player.weight != undefined){
+            let weight = "WEIGHT: <span class='answer'>";
 
-        weight += data.player.weight;
-        weight += "</span><br>";
+            weight += data.player.weight;
+            weight += "</span><br>";
 
-        basicInfo.innerHTML += weight;
-    }
+            basicInfo.innerHTML += weight;
+        }
 
-    if(data.player.birth.date != undefined){
-        let birthday = "BIRTHDAY: <span class='answer'>";
+        if(data.player.birth.date != undefined){
+            let birthday = "BIRTHDAY: <span class='answer'>";
 
-        birthday += data.player.birth.date;
-        birthday += "</span><br>";
+            birthday += data.player.birth.date;
+            birthday += "</span><br>";
 
-        basicInfo.innerHTML += birthday;
-    }
+            basicInfo.innerHTML += birthday;
+        }
 
-    if(basicInfo.innerHTML.trim() === ""){
-        basicInfo.style.color = 'red';
-        basicInfo.innerHTML = "No Advanced Information Found";
+        if(basicInfo.innerHTML.trim() === ""){
+            basicInfo.style.color = 'red';
+            basicInfo.innerHTML = "No Basic Information Found";
+        }
     }
 
     //-------------- DISPLAY BASIC STATS --------------//
 
     if (!data || !data.statistics) {
         basicStats.style.color = 'red';
-        basicStats.innerHTML = "No Advanced Information Found";
-        return;
-    }
+        basicStats.innerHTML = "No Basic Stats Found";
+    } else {
+        basicStats.innerHTML = "<span class='title'>BASIC STATS: </span><br><br>";
 
-    basicStats.innerHTML = "<span class='title'>BASIC STATS: </span><br><br>";
+        let caps = 0;
+        let minutes = 0;
+        let goals = 0;
+        let assists = 0;
+        let rating = 0.0;
 
-    let caps = 0;
-    let minutes = 0;
-    let goals = 0;
-    let assists = 0;
-    let rating = 0.0;
+        for(let i = 0; i < data.statistics.length; i++){
+            if(data.statistics[i].games.appearences != undefined){
+                caps += data.statistics[i].games.appearences;
 
-    for(let i = 0; i < data.statistics.length; i++){
-        if(data.statistics[i].games.appearences != undefined){
-            caps += data.statistics[i].games.appearences;
+                if(data.statistics[i].games.rating != null){
+                    rating += data.statistics[i].games.appearences * data.statistics[i].games.rating;;
+                }
+            }
 
-            if(data.statistics[i].games.rating != null){
-                rating += data.statistics[i].games.appearences * data.statistics[i].games.rating;;
+            if(data.statistics[i].games.minutes != undefined){
+                minutes += data.statistics[i].games.minutes;
+            }
+
+            if(data.statistics[i].goals.total != undefined){
+                goals += data.statistics[i].goals.total;
+            }
+
+            if(data.statistics[i].goals.assists != undefined){
+                assists += data.statistics[i].goals.assists;
             }
         }
 
-        if(data.statistics[i].games.minutes != undefined){
-            minutes += data.statistics[i].games.minutes;
+        if(caps != 0){
+            rating /= caps;
+            rating = rating.toFixed(2);
         }
 
-        if(data.statistics[i].goals.total != undefined){
-            goals += data.statistics[i].goals.total;
+        let appearanceText = "APPEARANCES: <span class='answer'>";
+        appearanceText += caps;
+        appearanceText += "</span><br>";
+        basicStats.innerHTML += appearanceText;
+
+        let minuteText = "MINUTES: <span class='answer'>";
+        minuteText += minutes;
+        minuteText += "</span><br>";
+        basicStats.innerHTML += minuteText;
+
+        let goalText = "GOALS: <span class='answer'>";
+        goalText += goals;
+        goalText += "</span><br>";
+        basicStats.innerHTML += goalText;
+
+        let assistText = "ASSISTS: <span class='answer'>";
+        assistText += assists;
+        assistText += "</span><br>";
+        basicStats.innerHTML += assistText;
+
+        let ratingText = "AVERAGE RATING: <span class='answer'>";
+        ratingText += rating;
+        ratingText += "</span><br>";
+        basicStats.innerHTML += ratingText;
+
+        if(basicStats.innerHTML.trim() === ""){
+            basicStats.style.color = 'red';
+            basicStats.innerHTML = "No Basic Stats Found";
         }
-
-        if(data.statistics[i].goals.assists != undefined){
-            assists += data.statistics[i].goals.assists;
-        }
-    }
-
-    if(caps != 0){
-        rating /= caps;
-        rating = rating.toFixed(2);
-    }
-
-    let appearanceText = "APPEARANCES: <span class='answer'>";
-    appearanceText += caps;
-    appearanceText += "</span><br>";
-    basicStats.innerHTML += appearanceText;
-
-    let minuteText = "MINUTES: <span class='answer'>";
-    minuteText += minutes;
-    minuteText += "</span><br>";
-    basicStats.innerHTML += minuteText;
-
-    let goalText = "GOALS: <span class='answer'>";
-    goalText += goals;
-    goalText += "</span><br>";
-    basicStats.innerHTML += goalText;
-
-    let assistText = "ASSISTS: <span class='answer'>";
-    assistText += assists;
-    assistText += "</span><br>";
-    basicStats.innerHTML += assistText;
-
-    let ratingText = "AVERAGE RATING: <span class='answer'>";
-    ratingText += rating;
-    ratingText += "</span><br>";
-    basicStats.innerHTML += ratingText;
-
-    if(basicStats.innerHTML.trim() === ""){
-        basicStats.style.color = 'red';
-        basicStats.innerHTML = "No Advanced Stats Found";
     }
 
     //-------------- DISPLAY HEADSHOT --------------//
 
     const headshot = document.getElementById('headshot');
-    headshot.src = data.player.photo;
+
+    if(!data || !data.player || !data.player.photo){
+        headshot.src = 'https://media.api-sports.io/football/players/434267.png';
+    } else {
+        headshot.src = data.player.photo;
+    }
 }
 
 function resizeScreen(){
