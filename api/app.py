b4059d9f2b10cd0455e74219cf4af93cc408da81
lucasolmeta <lolmetab@gmail.com>
2024-11-13 03:04:46 -0600

    bug fixes

--- api/app.py
+++ api/app.py
@@ -95,11 +95,11 @@ def get_data_by_id(id):
 
     seasons = get_seasons_for_player(id)
 
-    # return jsonify({"seasons":seasons,"type":type(seasons)})
+    return {"seasons":seasons,"type":type(seasons)}
 
     recent_season = seasons.response[-1]
 
-    return seasons.response[-1]
+    return seasons[-1]
 
     url = "https://api-football-v1.p.rapidapi.com/v3/players"
 
