56a55f4f1033371f284e8ac2e586875bf932dba6
lucasolmeta <lolmetab@gmail.com>
2025-01-27 17:04:31 -0600

    added more front end error detection

--- front-end/resultsscript.mjs
+++ front-end/resultsscript.mjs
@@ -296,7 +296,7 @@ function displayGraph(){
 
     let imgData = "";
 
-    if(!isValidJSON(graphs) || graphs.goals == "error" || graphs.assists == "error" || graphs.games == "error" || graphs.ratings == "error"){
+    if(graphs.goals == "error" || graphs.assists == "error" || graphs.games == "error" || graphs.ratings == "error"){
         const sectionTwo = document.getElementById('section2');
         sectionTwo.remove();
 
